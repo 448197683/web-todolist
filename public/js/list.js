4217baaa855bf6d19ca63e0fe183c165f7f16452
448197683 <448197683@qq.com>
2021-11-18 15:27:36 +0800

    Update list.js

--- public/js/list.js
+++ public/js/list.js
@@ -1,7 +1,13 @@
 const linkBtns = document.querySelectorAll('.link');
+const linkContainer = document.querySelector('.list-group');
+console.log(linkContainer);
 
-linkBtns.forEach((linkBtn) => {
+let selected = null;
+let target = null;
+
+/* linkBtns.forEach((linkBtn) => {
   linkBtn.addEventListener('click', async (e) => {
+
     e.preventDefault();
     try {
       const descFetch = await fetch(
@@ -13,5 +19,33 @@ linkBtns.forEach((linkBtn) => {
     } catch (error) {
       console.log(error);
     }
+  }); 
+}); */
+
+linkBtns.forEach((linkBtn) => {
+  linkBtn.addEventListener('dragstart', () => {
+    selected = linkBtn;
+  });
+  linkBtn.addEventListener('dragend', () => {
+    if (selected.nextElementSibling == target) {
+      console.log(`下一个`);
+      return target.after(selected);
+    }
+    if (target.nextElementSibling == null) {
+      console.log(`最后一个`);
+      return target.after(selected);
+    }
+    if (target == selected.previousElementSibling) {
+      console.log(`上一个`);
+      return target.before(selected);
+    }
+    if (target.previousElementSibling == null) {
+      console.log(`第一个`);
+      return target.before(selected);
+    }
+    linkContainer.insertBefore(selected, target.nextElementSibling);
+  });
+  linkBtn.addEventListener('dragenter', () => {
+    target = linkBtn;
   });
 });
